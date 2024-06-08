8ab0d07ad19a6bcf0eccf7c9cd0c53c2e960f824
illiabranchuk <jointlord0@gmail.com>
2024-06-08 12:48:41 +0100

    MA-3 Create Capture Moles Photo Screen

--- src/themes/Images.tsx
+++ src/themes/Images.tsx
@@ -1,4 +1,6 @@
 export default {
   iconClose: require("../assets/images/ic_close.png"),
   iconScan: require("../assets/images/ic_scan.png"),
+  iconGalary: require("../assets/images/ic_galary.png"),
+  iconCamera: require("../assets/images/ic_camera.png"),
 };
